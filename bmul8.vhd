57df09dc423d3f5433cb5d06c557f529eabdd39f
Nlipski <13ml76@queensu.ca>
2017-02-22 04:04:31 -0500

    booth

--- bmul8.vhd
+++ bmul8.vhd
@@ -1,7 +1,7 @@
--- bmul32.vhdl parallel multiply 32 bit x 32 bit two's complement
--- the main components are bmul32, special Booth 32 x 32 -> 16 bit multiplier
+-- bmul8.vhdl parallel multiply 32 bit x 32 bit two's complement
+-- the main components are bmul8, special Booth 32 x 32 -> 16 bit multiplier
 -- badd8  32 bit specialized  adder for Booth multiplier
--- needs add32 and fadd components in WORK
+-- needs add8 and fadd components in WORK
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -56,31 +56,31 @@ begin  -- circuits of badd8
   a1: entity add8 port map(sum_in, bb, cin, psum, cout);
   a2: entity fadd port map(sum_in(31), topbit, cout, topout, nc1);
 
-  sum_out(29 downto 0) <= psum(31 downto 2);
-  sum_out(31) <= topout;
-  sum_out(30) <= topout;
+  sum_out(5 downto 0) <= psum(7 downto 2);
+  sum_out(7) <= topout;
+  sum_out(6) <= topout;
   prod <= psum(1 downto 0);
 end architecture circuits;  -- of badd8
 
--- bmul32   full combinatorial 32 X 32 = 64 bit two's complement multiplier
+-- bmul8   full combinatorial 32 X 32 = 64 bit two's complement multiplier
 -- Booth two's complement multiplication using badd4 component
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity bmul8 is   -- 32-bit by 32-bit two's complement multiplier
-  port (a : in  std_logic_vector(31 downto 0);   -- multiplier
-        b : in  std_logic_vector(31 downto 0);   -- multiplicand
-        p : out std_logic_vector(63 downto 0));  -- product
+entity bmul8 is  
+  port (a : in  std_logic_vector(7 downto 0);   -- multiplier
+        b : in  std_logic_vector(7 downto 0);   -- multiplicand
+        p : out std_logic_vector(15 downto 0));  -- product
 end entity bmul8;
 
 architecture circuits of bmul8 is
-  signal zer : std_logic_vector(31 downto 0) := x"00000000";     -- zeros
+  signal zer : std_logic_vector(7 downto 0) := x"00";     -- zeros
   signal mul0: std_logic_vector(2 downto 0);
-  subtype word is std_logic_vector(31 downto 0);
-  type ary is array(0 to 15) of word;
+  subtype word is std_logic_vector(7 downto 0);
+  type ary is array(0 to 3) of word;
   signal s : ary;                       -- temp sums
-begin  -- circuits of bmul32
+begin  -- circuits of bmul8
   mul0 <= a(1 downto 0) & '0';
   a0:  entity badd8 port map(
               mul0,            b, zer,   s( 0), p( 1 downto  0));
@@ -90,31 +90,7 @@ begin  -- circuits of bmul32
               a(5  downto  3), b, s( 1), s( 2), p( 5 downto  4));
   a3:  entity badd8 port map(
               a(7  downto  5), b, s( 2), s( 3), p( 7 downto  6));
-  a4:  entity badd8 port map(
-              a(9  downto  7), b, s( 3), s( 4), p( 9 downto  8));
-  a5:  entity badd8 port map(
-              a(11 downto  9), b, s( 4), s( 5), p(11 downto 10));
-  a6:  entity badd8 port map(
-              a(13 downto 11), b, s( 5), s( 6), p(13 downto 12));
-  a7:  entity badd8 port map(
-              a(15 downto 13), b, s( 6), s( 7), p(15 downto 14));
-  a8:  entity badd8 port map(
-              a(17 downto 15), b, s( 7), s( 8), p(17 downto 16));
-  a9:  entity badd8 port map(
-              a(19 downto 17), b, s( 8), s( 9), p(19 downto 18));
-  a10: entity badd8 port map(
-              a(21 downto 19), b, s( 9), s(10), p(21 downto 20));
-  a11: entity badd8 port map(
-              a(23 downto 21), b, s(10), s(11), p(23 downto 22));
-  a12: entity badd8 port map(
-              a(25 downto 23), b, s(11), s(12), p(25 downto 24));
-  a13: entity badd8 port map(
-              a(27 downto 25), b, s(12), s(13), p(27 downto 26));
-  a14: entity badd8 port map(
-              a(29 downto 27), b, s(13), s(14), p(29 downto 28));
-  a15: entity badd8 port map(
-              a(31 downto 29), b, s(14), p(63 downto 32) , p(31 downto 30));
-end architecture circuits;  -- of bmul32
+end architecture circuits;  -- of bmul8
 
 
 
@@ -135,16 +111,16 @@ end architecture circuits; -- of fadd
 
 library IEEE;
 use IEEE.std_logic_1164.all;
-entity add32 is             -- simple 32 bit ripple carry adder
-  port(a    : in  std_logic_vector(31 downto 0);
-       b    : in  std_logic_vector(31 downto 0);
+entity add8 is             -- simple 8 bit ripple carry adder
+  port(a    : in  std_logic_vector(7 downto 0);
+       b    : in  std_logic_vector(7 downto 0);
        cin  : in  std_logic; 
-       sum  : out std_logic_vector(31 downto 0);
+       sum  : out std_logic_vector(7 downto 0);
        cout : out std_logic);
-end entity add32;
+end entity add8;
 
-architecture circuits of add32 is
-  signal c : std_logic_vector(0 to 30); -- internal carry signals
+architecture circuits of add8 is
+  signal c : std_logic_vector(0 to 6); -- internal carry signals
   component fadd   -- duplicates entity port
   port(a    : in  std_logic;
        b    : in  std_logic;
@@ -152,10 +128,10 @@ architecture circuits of add32 is
        s    : out std_logic;
        cout : out std_logic);
   end component fadd ;
-begin  -- circuits of add32
+begin  -- circuits of add8
   a0:            fadd port map(a(0), b(0), cin, sum(0), c(0));
-  stage: for I in 1 to 30 generate
+  stage: for I in 1 to 6 generate
              as: fadd port map(a(I), b(I), c(I-1) , sum(I), c(I));
          end generate stage;
-  a31:           fadd port map(a(31), b(31), c(30) , sum(31), cout);
-end architecture circuits;  -- of add32
+  a31:           fadd port map(a(7), b(7), c(6) , sum(7), cout);
+end architecture circuits;  -- of add8
